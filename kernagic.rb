e6ee32824d8d9653ee78c5817d144903ec817d70
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-06-13 07:19:29 +0100

    kernagic went quartz-only
    revision bump
    audit --strict compliance

--- kernagic.rb
+++ kernagic.rb
@@ -1,14 +1,13 @@
-require 'formula'
-
 class Kernagic < Formula
-  homepage 'https://github.com/hodefoting/kernagic/'
-  url 'https://github.com/hodefoting/kernagic/archive/v0.2.tar.gz'
-  sha1 '6099408fc9ebe22936b048364a8fcd7b5b66a1d8'
-  head 'https://github.com/hodefoting/kernagic.git'
+  desc "Semi-automatic font spacing tool"
+  homepage "https://github.com/hodefoting/kernagic/"
+  url "https://github.com/hodefoting/kernagic/archive/v0.2.tar.gz"
+  sha256 "41c328a2c177fd2c41f094641cdce4931115c34e69f140512d1461d42da502c2"
+  head "https://github.com/hodefoting/kernagic.git"
+  revision 1
 
-  depends_on :x11
-  depends_on 'pkg-config' => :build
-  depends_on 'gtk+'
+  depends_on "pkg-config" => :build
+  depends_on "gtk+"
 
   def install
     system "make"
@@ -16,6 +15,6 @@ class Kernagic < Formula
   end
 
   test do
-    system "#{bin}/kernagic -h"
+    system "#{bin}/kernagic", "-h"
   end
 end
