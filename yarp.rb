013bb7a060e64218de23d4b6a67e3fb655942e55
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-06-13 18:09:50 +0100

    yarp went quartz-only
    revision bump
    audit --strict compliance

--- yarp.rb
+++ yarp.rb
@@ -1,10 +1,10 @@
-require "formula"
-
 class Yarp < Formula
+  desc "Yet Another Robot Platform"
   homepage "http://yarp.it"
-  head "https://github.com/robotology/yarp.git"
   url "https://github.com/robotology/yarp/archive/v2.3.63.tar.gz"
-  sha1 "bef87cf5e53c1dc5b9fe4de90022cb8285405fa2"
+  sha256 "1ee3b237b8700f55deaaae9be46015bfb6d454135ab99d096d269cea6e3d7d3e"
+  head "https://github.com/robotology/yarp.git"
+  revision 1
 
   option "without-shared", "Build only static version of YARP libraries"
 
@@ -18,35 +18,35 @@ class Yarp < Formula
   depends_on "sqlite"
   depends_on "readline"
   depends_on "jpeg"
-  depends_on :x11
 
   def install
-    args = std_cmake_args + %W[
-      -DCREATE_LIB_MATH=TRUE
-      -DCREATE_GUIS=TRUE
-      -DCREATE_YMANAGER=TRUE
-      -DYARP_USE_SYSTEM_SQLITE=TRUE
-      -DCREATE_OPTIONAL_CARRIERS=TRUE
-      -DENABLE_yarpcar_mjpeg_carrier=TRUE
-      -DENABLE_yarpcar_rossrv_carrier=TRUE
-      -DENABLE_yarpcar_tcpros_carrier=TRUE
-      -DENABLE_yarpcar_xmlrpc_carrier=TRUE
-      -DENABLE_yarpcar_bayer_carrier=TRUE
-      -DUSE_LIBDC1394=FALSE
-      -DENABLE_yarpcar_priority_carrier=TRUE
-      -DCREATE_IDLS=TRUE
-      -DENABLE_yarpidl_thrift=TRUE
-      -DCREATE_YARPVIEW=TRUE
-      -DCREATE_YARPSCOPE=TRUE
-      -DCREATE_GYARPMANAGER=TRUE
-      ]
+    args = std_cmake_args +
+           %W[
+             -DCREATE_LIB_MATH=TRUE
+             -DCREATE_GUIS=TRUE
+             -DCREATE_YMANAGER=TRUE
+             -DYARP_USE_SYSTEM_SQLITE=TRUE
+             -DCREATE_OPTIONAL_CARRIERS=TRUE
+             -DENABLE_yarpcar_mjpeg_carrier=TRUE
+             -DENABLE_yarpcar_rossrv_carrier=TRUE
+             -DENABLE_yarpcar_tcpros_carrier=TRUE
+             -DENABLE_yarpcar_xmlrpc_carrier=TRUE
+             -DENABLE_yarpcar_bayer_carrier=TRUE
+             -DUSE_LIBDC1394=FALSE
+             -DENABLE_yarpcar_priority_carrier=TRUE
+             -DCREATE_IDLS=TRUE
+             -DENABLE_yarpidl_thrift=TRUE
+             -DCREATE_YARPVIEW=TRUE
+             -DCREATE_YARPSCOPE=TRUE
+             -DCREATE_GYARPMANAGER=TRUE
+           ]
 
-      if build.without? "shared"
-        args << "-DCREATE_SHARED_LIBRARY:BOOL=FALSE"
-      else
-        args << "-DCREATE_SHARED_LIBRARY:BOOL=TRUE"
-      end
-      args << "."
+    if build.without? "shared"
+      args << "-DCREATE_SHARED_LIBRARY:BOOL=FALSE"
+    else
+      args << "-DCREATE_SHARED_LIBRARY:BOOL=TRUE"
+    end
+    args << "."
 
     system "cmake", *args
     system "make", "install"
